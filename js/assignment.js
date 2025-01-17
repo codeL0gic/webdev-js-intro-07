897632934a4edcf0f63e2f856f6df9897e70500f
codeL0gic <shannon.neumann@yum.com>
2025-01-17 13:25:54 -0500

    main

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,29 @@ const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+function returnTheRemaider() {
+  let remainder = 5 % 4;
+  remainderElement.innerHTML = remainder;
+}
+
+function checkIfEven() {
+  let remainder = 5 % 4;
+  let isEven = remainder === 0;
+  checkIfEvenElement.innerHTML = isEven;
+}
+function getTheFourthElement() {
+  let fourthElement = lostNumbers[3];
+  lostNumbersElement.innerHTML = fourthElement;
+}
+
+function render() {
+  // Call the created functions
+
+  returnTheRemaider();
+  checkIfEven();
+  getTheFourthElement();
+}
+submissionBtn.addEventListener("click", function () {
+  render();
+});
--- js/index.js
+++ js/index.js
@@ -1,4 +1,48 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+"use strict";
 
+//  function greet() {
+//    console.log("Hello, World!");
+//  }
+
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+// let sum = add(2, 4);
+// console.log(sum);
+
+// function greet(name = 'Guest') {
+
+//   console.log(`Hello, '+ name + '!');
+// }
+// greet();
+// greet("John");
+
+// const greet = (anme) => {
+//     return "Hello, ' + name + '!";
+//     };
+
+//     const greet = (num1, num2) => a + b;
+//     let sum = add(2, 4);
+//     console.log(sum);
+
+function walk() {
+  console.log(fast);
+}
+
+function address(house1, house2) {
+  let house1Address = 4028;
+  let house2Address = 4029;
+  return house1Address + house2Address;
+}
+let sum = address(4028, 4029);
+console.log(sum);
+
+function address(house3, house4) {
+  let house3Address = 10;
+  let house4Address = 20;
+  let sum = house3Address + house4Address;
+  return house3Address + house4Address;
+}
+console.log("I am Home " + sum);
